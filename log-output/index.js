74fb9aaa75525c5d1179b93c6de5a06cf1a037a6
nikogenix <nikogenix@gmail.com>
2023-06-30 05:44:52 +0300

    Completing 1.07

--- log-output/index.js
+++ log-output/index.js
@@ -1,11 +1,27 @@
+const express = require("express");
 const { v4: uuidv4 } = require("uuid");
+process.env.NODE_ENV && require("dotenv").config();
 
-const randomString = uuidv4();
+const app = express();
+const port = process.env.PORT || 3003;
+
+const info = {
+	randomString: uuidv4(),
+	timestamp: new Date().toISOString(),
+};
 
 const output = () => {
-	const timestamp = new Date().toISOString();
-	console.log(`${timestamp}: ${randomString}`);
-	setInterval(output, 5000);
+	info.timestamp = new Date().toISOString();
+	console.log(`${info.timestamp}: ${info.randomString}`);
+	setTimeout(output, 5000);
 };
 
 output();
+
+app.listen(port, () => {
+	console.log(`server started at http://localhost:${port}/`);
+});
+
+app.get("/info", (req, res) => {
+	res.send(`<h1>${info.timestamp}: ${info.randomString}</h1>`);
+});
