67b3fc69688944b7b5200d86d30c840fee146f92
nikogenix <nikogenix@gmail.com>
2023-06-30 04:17:09 +0300

    Completing 1.05

--- project/index.js
+++ project/index.js
@@ -5,5 +5,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-	console.log(`server started in port ${port}`);
+	console.log(`server started at http://localhost:${port}/`);
+});
+
+app.get("/", (req, res) => {
+	res.send("<h1>hello kube</h1>");
 });
