f8cf0c1d68ef0b0c2c0b8866f02108e5eb6cbd05
nikogenix <nikogenix@gmail.com>
2023-07-04 06:00:57 +0300

    Completing 2.01

--- log-output/log-reader/index.js
+++ log-output/log-reader/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
 process.env.NODE_ENV && require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3006;
 
 const directory = path.join("/", "usr", "src", "app", "files");
 const filePath = path.join(directory, "log.txt");
-const pingFilePath = path.join(directory, "ping.txt");
 
 const getFile = async () =>
 	new Promise((res) => {
@@ -19,30 +18,28 @@ const getFile = async () =>
 		});
 	});
 
-const getPingFile = async () =>
-	new Promise((res) => {
-		fs.readFile(pingFilePath, (err, buffer) => {
-			if (err) return console.log(err);
-			res(buffer);
-		});
-	});
-
+const getPing = async () => {
+	const response = await axios.get("http://ping-pong-svc:3011");
+	return response.data;
+};
 let info = "";
 let ping = "";
 
 const output = async () => {
 	info = await getFile();
-	ping = await getPingFile();
+	ping = await getPing();
 	console.log(info, ping);
 	setTimeout(output, 5000);
 };
 
 output();
 
+app.use(express.json());
+
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}/`);
 });
 
 app.get("/info", (req, res) => {
-	res.send(`<h1>${info}</h1><h1>${[ping]}</h1>`);
+	res.send(`<h1>${info}</h1><h1>ping/pongs: ${ping}</h1>`);
 });
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,39 +1,23 @@
 const express = require("express");
 process.env.NODE_ENV && require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3005;
 
-const directory = path.join("/", "usr", "src", "app", "files");
-const filePath = path.join(directory, "ping.txt");
-
-const fileAlreadyExists = async () =>
-	new Promise((res) => {
-		fs.stat(directory, (err, stats) => {
-			if (err || !stats) return res(false);
-			return res(true);
-		});
-	});
-
-const output = async (ping) => {
-	if (!(await fileAlreadyExists())) {
-		await new Promise((res) => fs.mkdir(directory, (err) => console.log(err)));
-	}
-
-	fs.writeFile(filePath, `ping-pongs: ${ping}`, (err) => err && console.log(err));
-};
+let ping = 0;
 
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}/`);
 });
 
-let ping = 0;
-output(ping);
+app.use(cors());
 
 app.get("/pingpong", (req, res) => {
 	ping++;
-	output(ping);
 	res.send(`<h1>ping-pongs: ${ping}</h1>`);
+});
+
+app.get("/", (req, res) => {
+	res.send(`${ping}`);
 });
