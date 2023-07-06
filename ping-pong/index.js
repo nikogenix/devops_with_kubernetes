d35d9a44438fbe93c459bdb9e1147108f3f87f16
nikogenix <nikogenix@gmail.com>
2023-07-06 08:33:19 +0300

    Completing 2.07

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -5,7 +5,92 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3005;
 
-let ping = 0;
+const { Client } = require("pg");
+
+let client = new Client({
+	connectionString: process.env.DB_URL,
+});
+
+async function connectDb() {
+	while (true) {
+		const tempClient = new Client({
+			connectionString: process.env.DB_URL,
+		});
+
+		try {
+			await tempClient.connect();
+			console.log("connected to the DB");
+			client = tempClient;
+			break;
+		} catch (error) {
+			console.error(error.message);
+			await new Promise((resolve) => setTimeout(resolve, 5000));
+		}
+	}
+}
+
+async function checkAndIncrementPing() {
+	try {
+		const tableExists = await client.query(
+			`SELECT EXISTS (
+				SELECT 1
+				FROM information_schema.tables
+				WHERE table_schema = 'public'
+				AND table_name = 'ping_table'
+      		);`
+		);
+
+		if (!tableExists.rows[0].exists) {
+			await client.query(
+				`CREATE TABLE ping_table (
+					ping INT
+				);`
+			);
+
+			await client.query("INSERT INTO ping_table (ping) VALUES (1)");
+			return 1;
+		}
+
+		await client.query("UPDATE ping_table SET ping = ping + 1");
+		const result = await client.query("SELECT ping FROM ping_table LIMIT 1");
+		const ping = result.rows[0].ping;
+		return ping;
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+async function checkPing() {
+	try {
+		const tableExists = await client.query(
+			`SELECT EXISTS (
+				SELECT 1
+				FROM information_schema.tables
+				WHERE table_schema = 'public'
+				AND table_name = 'ping_table'
+      		);`
+		);
+
+		if (!tableExists.rows[0].exists) {
+			await client.query(
+				`CREATE TABLE ping_table (
+					ping INT
+				);`
+			);
+
+			await client.query("INSERT INTO ping_table (ping) VALUES (1)");
+			return 1;
+		}
+
+		const result = await client.query("SELECT ping FROM ping_table LIMIT 1");
+		const ping = result.rows[0].ping;
+		return ping;
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+connectDb();
 
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}/`);
@@ -13,11 +98,22 @@ app.listen(port, () => {
 
 app.use(cors());
 
-app.get("/pingpong", (req, res) => {
-	ping++;
-	res.send(`<h1>ping-pongs: ${ping}</h1>`);
+app.get("/pingpong", async (req, res) => {
+	try {
+		const ping = await checkAndIncrementPing();
+		res.send(`<h1>ping-pongs: ${ping}</h1>`);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("DB error");
+	}
 });
 
-app.get("/", (req, res) => {
-	res.send(`${ping}`);
+app.get("/", async (req, res) => {
+	try {
+		const ping = await checkPing();
+		res.send(`${ping}`);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("DB error");
+	}
 });
